6f3aee7185242f04eefaeb2a7e922dcedcc41465
Hai Trinh Tien <trinhtienhai@gmail.com>
2021-05-09 21:17:22 +1200

    feat(setup): ios and android project

---
+++ ios/FileAsset.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface FileAsset : NSObject <RCTBridgeModule>
+
+@end
---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,5 @@
+declare type FileAssetType = {
+    multiply(a: number, b: number): Promise<number>;
+};
+declare const _default: FileAssetType;
+export default _default;
